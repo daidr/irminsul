de50676e26b8af5e2a3e7d5aecf59e7c0b2235eb
Xuezhou Dai <daidr@daidr.me>
2026-04-11 01:26:02 +0800

    feat(oauth): add BuiltInIcon island server component

---
+++ components/islands/BuiltInIcon.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { HugeiconsIcon } from "@hugeicons/vue";
+
+const props = defineProps<{
+  name: string;
+  size?: number;
+}>();
+
+const icon = resolveBuiltInIcon(props.name);
+</script>
+
+<template>
+  <HugeiconsIcon :icon="icon" :size="size ?? 24" style="color: var(--theme-fg)" />
+</template>
